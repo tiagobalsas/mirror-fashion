b0ed1a272884c388825d8b2bc051c6293b207f4d
tiagobalsas <tiago881@gmail.com>
2020-05-22 15:16:41 -0300

    novos banner

--- js/banner.js
+++ js/banner.js
@@ -1,8 +1,8 @@
-var banners = ["img/destaque-home.png", "img/destaque-home-2.png"];
+var banners = ["img/destaque-home.png", "img/destaque-home1.png", "img/destaque-home2.png", "img/destaque-home3.png","img/destaque-home4.png", "img/destaque-home5.png", "img/destaque-home6.png", "img/destaque-home7.png"  ];
 var bannerAtual = 0;
 
 function trocaBanner() {
-    bannerAtual = (bannerAtual + 1) % 2;
+    bannerAtual = (bannerAtual + 1) % 8;
     document.querySelector('.banner-destaque img ').src = banners [bannerAtual];
 }
 
